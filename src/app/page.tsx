be1c904c719309c5a56c56d9d4e82dcbf35b2387
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-10-01 23:18:00 +0530

    Dynamic Class Max Width Wrapper

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import MaxWidthWrapper from "@/app/components/Wrapper/MaxWidthWrapper";
+
+export default function Home() {
+  return (
+    <main>
+      <MaxWidthWrapper className="bg-red-500">Page</MaxWidthWrapper>
+    </main>
+  );
+}
